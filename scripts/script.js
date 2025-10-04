ad12535fdbe284dea7536a19ac9e5cbb94e627c1
LeenaZir <valerietatarenko664@gmail.com>
2025-10-04 16:22:46 +0300

    1.2 code

---
+++ scripts/script.js
@@ -0,0 +1,16 @@
+const paragraphs = document.querySelectorAll('p');
+console.log('<p> count: ', paragraphs.length);
+
+const h2Elements = document.querySelectorAll('h2');
+console.log('<h2> count: ', h2Elements.length);
+
+const bodyBgColor = window.getComputedStyle(document.body).backgroundColor;
+console.log('background-color <body>: ', bodyBgColor);
+
+const h1 = document.querySelector('h1');
+if (h1) {
+    const h1FontSize = window.getComputedStyle(h1).fontSize;
+    console.log('font-size <h1>: ', h1FontSize);
+} else {
+    console.log('No <h1> on this page.');
+}
